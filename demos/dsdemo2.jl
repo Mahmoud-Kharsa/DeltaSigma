5a90e9d1c3956e0c152a97a4ab8f6a0fcf11247d
Jafeer Uddin <jafeer.uddin@gmail.com>
2020-01-29 02:46:34 -0500

    implement first graph of dsdemo2 using simulateDSM

---
+++ demos/dsdemo2.jl
@@ -0,0 +1,21 @@
+using ControlSystems
+using DeltaSigma
+using MAT
+using Plots
+
+OSR = 32
+# use output from MATLAB to bypass synthesizeNTF for now
+# H = synthesizeNTF(5,OSR,1)
+vars = matread("test/simulateDSM.mat")
+H = zpk(vars["H_z"][:,1], vars["H_p"][:,1], 1, 1)
+N = 8192
+fB = ceil(N/(2*OSR))
+ftest=floor(2/3*fB)
+u = 0.5*sin.(2*pi*ftest/N*(0:N-1)')
+v, = simulateDSM(u, H)
+
+t = 0:100
+plot(t, u[t .+ 1], line=:red, linetype=:steppre, legend=false)
+plot!(t, v[t .+ 1], line=:green, linetype=:steppre, legend=false)
+xlabel!("Sample Number")
+ylabel!("u, v")
